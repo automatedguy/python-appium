a21b6bfd4d577aa32172f182667406b45a0db3c6
Gabriel Cespedes <Gabriel Cespedes>
2017-10-02 19:44:14 -0300

    Just minor report thing for testing.

--- tests/flights/booking.py
+++ tests/flights/booking.py
@@ -10,7 +10,7 @@ class BookingTest(BaseTest):
 
     def test_the_first_one(self):
         pass
-        self.logger.info("Test 1")
+        self.logger.info("Starting Test 1")
         self.logger.warn("Test 1")
         self.logger.error("Test 1")
         self.logger.critical("Test 1")
@@ -18,7 +18,7 @@ class BookingTest(BaseTest):
 
     def test_the_second_one(self):
         pass
-        self.logger.info("Test 2")
+        self.logger.info("Starting Test 2")
         self.logger.warn("Test 2")
         self.logger.error("Test 2")
         self.logger.critical("Test 2")
@@ -26,7 +26,7 @@ class BookingTest(BaseTest):
 
     def test_the_third_one(self):
         pass
-        self.logger.info("Test 3")
+        self.logger.info("Starting Test 3")
         self.logger.warn("Test 3")
         self.logger.error("Test 3")
         self.logger.critical("Test 3")

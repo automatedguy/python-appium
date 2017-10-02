2fb5ce19993db69f8d5b0bcd3de0301c7334d84b
Gabriel Cespedes <Gabriel Cespedes>
2017-10-01 23:55:17 -0300

    Added saucelabs const module

---
+++ app/constants/saucelabs.py
@@ -0,0 +1,7 @@
+USER_NAME = "viajesyplacerautomation"
+ACCESS_KEY = "1deb3616-b504-4e7d-ad44-da6108d4f7c9"
+HTTP_URL = "http://"
+LOCAL_URL = "localhost:4723/wd/hub"
+HTTPS_URL = "https://"
+SAUCELABS_URL = "@ondemand.saucelabs.com:443/wd/hub"
+
